d5508aca5f940d657ed7b45876e3012de1a6a6e7
MallampatiBhavishyaChowdary <146431037+MallampatiBhavishyaChowdary@users.noreply.github.com>
2024-02-28 23:54:18 +0530

    Add files via upload

---
+++ Day-2.py
@@ -0,0 +1,6 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(
+...     "Some Amazing Projects:1. Jarvis Virtual Assistant2. love Calculator3. Face Recognition4. Flappy Bird Game5. Snake Game"
+... )
+Some Amazing Projects:1. Jarvis Virtual Assistant2. love Calculator3. Face Recognition4. Flappy Bird Game5. Snake Game
